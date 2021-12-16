4f2c4ef745c4c4a75c7f528f3935e35937d4ddff
eyalmol <eyalmola18@gmail.com>
2021-12-16 02:06:28 +0200

    setting_index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+//set up our express app
+const app = express();
+//connect to mongoDB
+mongoose.connect("mongodb://localhost:27017");
+mongoose.Promise = global.Promise;
+//add support to serve static files
+app.use(express.static("public"));
+//parse the data to json file
+app.use(express.json());
+//set the app to use the message.routes (will hold our operations)
+app.use("/message", require("./routes/message"));
+//handle errors that occurred during the operations
+app.use(function (err, req, res, next) {
+  res.status(422).send({ error: err.message });
+});
+//listen for requests
+app.listen(process.env.port || 4300, function () {
+  console.log("Ready to Go!");
+});
